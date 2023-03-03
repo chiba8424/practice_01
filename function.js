dbef0dc095d55e167dcba6c07fec6e6dcf0d63eb
DESKTOP-B10LJN0\jh_yang <jh.yang@coint.co.kr>
2023-03-03 11:19:26 +0900

    function

---
+++ function.js
@@ -0,0 +1,23 @@
+// test()
+
+(function () {
+    console.log("test")
+})()
+
+
+
+// 호이스팅(hoisting)
+// 익명함수(anomious function) : 함수명을 선언하지 않고, 바로 실행하는 함수
+// arrow function(화살표 함수)
+
+
+
+const test = function () {
+    console.log("test")
+}
+test()
+
+const test01 = () => ("end")
+
+
+console.log(test01())
